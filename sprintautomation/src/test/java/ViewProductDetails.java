e9c41a0c11f352f15ca4659e0b7c20cebcc544e2
Jonathan Colegrove <Demetrius472@gmail.com>
2022-01-14 18:56:18 -0500

    writing boilerplate

--- sprintautomation/src/test/java/ViewProductDetails.java
+++ sprintautomation/src/test/java/ViewProductDetails.java
@@ -6,7 +6,36 @@ import org.testng.annotations.AfterTest;
 public class ViewProductDetails {
 
 	final String URL = "http://automationpractice.com/";
-
+	final String PRINTED_SUMMER_DRESS = "http://automationpractice.com/index.php?id_product=6&controller=product";
+	  
+	  @Test
+	  public void canSeePriceCorrectly() {
+		  
+		  // Arrange
+		  
+		  // Act
+		  expectedPrice = "$30.50";
+		  actualPrice = span id="our_price_display" inner text();
+		  
+		  // Assert
+		  Assert.AssertNotNull(actualPrice);
+		  Assert.assertEquals(expectedPrice, actualPrice);
+	  }
+	  
+	  @Test
+	  public void canSeeProductTitle() {
+		  
+		  // Arrange
+		  
+		  // Act
+//		  expectedTitle = "Printed Summer Dress";
+		  actualTitle = #center_column > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1;
+		  
+		  // Assert
+		  Assert.AssertNotNull(actualPrice);
+//		  Assert.assertEquals(expectedPrice, actualPrice);
+	  }
+	  
 	@Test
 	public void canNavigateToURL() {
 

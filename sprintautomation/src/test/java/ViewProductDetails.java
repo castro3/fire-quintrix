b0e731c3ee49a644245884bb64c79f12119f32d5
Jonathan Colegrove <Demetrius472@gmail.com>
2022-01-14 18:56:06 -0500

    writing boilerplate

--- sprintautomation/src/test/java/ViewProductDetails.java
+++ sprintautomation/src/test/java/ViewProductDetails.java
@@ -1,28 +1,31 @@
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class ViewProductDetails {
-	
-	final String URL = "http://automationpractice.com/";
-	
-  @Test
-  public void canNavigateToURL() {
-	  
-	  // Arrange
-	  
-	  // Act
-	  
-	  // Assert
-	  
-  }
-  
-  @BeforeTest
-  public void beforeTest() {
-  }
 
-  @AfterTest
-  public void afterTest() {
-  }
+	final String URL = "http://automationpractice.com/";
+
+	@Test
+	public void canNavigateToURL() {
+
+		// Arrange
+
+		// Act
+
+		// Assert
+
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		driver.navigate().to(PRINTED_SUMMER_DRESS);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		cleanup();
+	}
 
 }

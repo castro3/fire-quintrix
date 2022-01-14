ca4d0c36f7e15120cf21ece8f682e1e1a6fef742
Jonathan Colegrove <Demetrius472@gmail.com>
2022-01-13 21:42:11 -0500

    Set up for new branch

---
+++ sprintautomation/src/test/java/ViewProductDetails.java
@@ -0,0 +1,28 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class ViewProductDetails {
+	
+	final String URL = "http://automationpractice.com/";
+	
+  @Test
+  public void canNavigateToURL() {
+	  
+	  // Arrange
+	  
+	  // Act
+	  
+	  // Assert
+	  
+  }
+  
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}

80fb3c4b4adf1cba0efe1239ce6bcba22778ab61
AnthonyA21 <95726846+AnthonyA21@users.noreply.github.com>
2022-01-15 11:02:29 -0600

    Adds AddToWishlist Test and class

---
+++ sprintautomation/src/test/java/sprint1Pages/AddToWishlist.java
@@ -0,0 +1,32 @@
+package sprint1Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import framework.AutomationPage;
+import framework.TestBase;
+
+public class AddToWishlist extends AutomationPage {
+	
+	//public ProductDetails productDetails;
+	
+	public AddToWishlist(WebDriver driver) {
+		super(driver);
+	}
+
+	public void navigateToBlouse() {
+		this.driver.findElement(By.xpath(".//a[@data-id-product='2']//following-sibling::a")).click();
+	}
+	
+	public void addBlouseToWishlist() {
+		WebElement addToWishlistLink = this.driver.findElement(By.xpath("//a[@id='wishlist_button']"));
+		addToWishlistLink.click();
+	}
+	
+	public String getMessageText() {
+		return this.driver.findElement(By.cssSelector("p[class='fancybox-error']")).getText();
+	}
+	
+
+}
---
+++ sprintautomation/src/test/java/sprint1Tests/AddToWishlistTest.java
@@ -0,0 +1,41 @@
+package sprint1Tests;
+
+import org.testng.annotations.Test;
+
+import framework.TestBase;
+import sprint1Pages.AddToWishlist;
+import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class AddToWishlistTest extends TestBase {
+	
+	public AddToWishlist addToWishlist;
+	
+	@Test
+	public void cannotAddToWishlistWithoutLogin() {
+		String expectedMessage = "You must be logged in to manage your wishlist.";
+		
+		addToWishlist.addBlouseToWishlist();
+		String actualMessage = addToWishlist.getMessageText();
+		
+		Assert.assertEquals(actualMessage, expectedMessage);
+		
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		this.setup();
+		getToDetailsPage();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		this.cleanup();
+	}
+
+	public void getToDetailsPage() {
+		addToWishlist = new AddToWishlist(this.getDriver());
+		addToWishlist.navigateToBlouse();
+	}
+}

8464223ce56b6e21f8e616d42f7a6599435bda59
Oscar <castroxoscar@gmail.com>
2022-01-22 09:53:52 -0700

    adds fixes

--- sprintautomation/src/test/java/sprint2Cucumber/loginCucumber/StepsLogin.java
+++ sprintautomation/src/test/java/sprint2Cucumber/loginCucumber/StepsLogin.java
@@ -1,6 +1,11 @@
 package sprint2Cucumber.loginCucumber;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
 import io.cucumber.java.en.And;
@@ -8,14 +13,24 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import sprint1Pages.UserLogin;
 
-public class StepsLogin extends LoginTestRunner{
+public class StepsLogin extends LoginTest{
 	
 	WebDriver driver;
 	UserLogin userLogin;
 
 	@Given("^user is at homepage$")
 	public void homepage() throws Throwable {
-		userLogin = new UserLogin(this.getDriver());
+		Path resourceDirectory = Paths.get("src","test","resources");
+		String absolutePath = resourceDirectory.toFile().getAbsolutePath();
+		Path driverFile = Paths.get(absolutePath, "chromedriver.exe");
+		
+		System.setProperty("webdriver.chrome.driver", driverFile.toFile().getAbsolutePath());
+
+		this.driver = new ChromeDriver();	
+		
+		long pageLoadTimeout = 5;
+		this.driver.manage().timeouts().implicitlyWait(pageLoadTimeout , TimeUnit.SECONDS);
+		userLogin = new UserLogin(this.driver);
 	}
 	
 	@Then("^click on login button$")

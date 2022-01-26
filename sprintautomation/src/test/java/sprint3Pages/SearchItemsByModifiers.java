4b63bbddaca91762a8b17cf28426387855f22bdb
Oscar <castroxoscar@gmail.com>
2022-01-25 19:30:11 -0700

    adds search by items and require login for checkout tests and page setup

---
+++ sprintautomation/src/test/java/sprint3Pages/SearchItemsByModifiers.java
@@ -0,0 +1,14 @@
+package sprint3Pages;
+
+import org.openqa.selenium.WebDriver;
+
+import framework.AutomationPage;
+
+public class SearchItemsByModifiers extends AutomationPage {
+
+	public SearchItemsByModifiers(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+}

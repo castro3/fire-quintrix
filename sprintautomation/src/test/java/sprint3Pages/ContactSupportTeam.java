8f6ee60b33a0687d9dc58286919c0e9595e70d25
Jonathan Colegrove <Demetrius472@gmail.com>
2022-01-25 20:05:06 -0500

    setup for Sprint 3

---
+++ sprintautomation/src/test/java/sprint3Pages/ContactSupportTeam.java
@@ -0,0 +1,5 @@
+package sprint3Pages;
+
+public class ContactSupportTeam {
+
+}
---
+++ sprintautomation/src/test/java/sprint3Tests/ContactSupportTeam.java
@@ -0,0 +1,24 @@
+package sprint3Tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class ContactSupportTeam {
+
+	@Test
+	public void canEmailSupportTeam() {
+
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+
+	}
+
+}

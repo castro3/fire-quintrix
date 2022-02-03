dbf564de2ef0a9d1469109268affa14358182dfa
Jonathan Colegrove <Demetrius472@gmail.com>
2022-02-02 20:41:59 -0500

    Confirmed Legal Notices are present

--- sprintautomation/src/test/java/sprint4Tests/LegalNoticeTest.java
+++ sprintautomation/src/test/java/sprint4Tests/LegalNoticeTest.java
@@ -1,19 +1,30 @@
 package sprint4Tests;
 
 import org.testng.annotations.Test;
+import framework.TestBase;
+import sprint4Pages.LegalNotice;
 import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
-public class LegalNoticeTest {
-  @Test
-  public void f() {
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
+public class LegalNoticeTest extends TestBase{
+	LegalNotice terms;
 
-  @AfterTest
-  public void afterTest() {
-  }
+	@BeforeTest
+	public void beforeTest() {
+		this.setup();
+		terms = new LegalNotice(this.getDriver());
+	}
 
+	@Test
+	public void findWhereTermsAreListed() {
+		terms.navigateToTermsPage();
+		String expectedTermRuleElements = "RULE 1 RULE 2 RULE 3";
+		Assert.assertEquals(terms.getTermRuleElements(), expectedTermRuleElements);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		this.cleanup();
+	}
 }

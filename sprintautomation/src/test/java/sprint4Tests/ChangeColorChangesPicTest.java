9f90fd216e11f9aec47294afcaf339a345f9c99e
Jonathan Colegrove <Demetrius472@gmail.com>
2022-02-07 21:14:43 -0500

    revised code to verify image changes when different color is selected

--- sprintautomation/src/test/java/sprint4Tests/ChangeColorChangesPicTest.java
+++ sprintautomation/src/test/java/sprint4Tests/ChangeColorChangesPicTest.java
@@ -2,19 +2,32 @@ package sprint4Tests;
 
 import org.testng.annotations.Test;
 import framework.TestBase;
+import sprint4Pages.ChangeColorChangesPic;
 import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class ChangeColorChangesPicTest extends TestBase{
 
+	ChangeColorChangesPic colors;
+	
 	@BeforeTest
 	public void beforeTest() {
 		this.setup();
+		colors = new ChangeColorChangesPic(this.getDriver());
 	}
 	
 	@Test
 	public void productImageChangesUponClickingColor() {
+		colors.navigateToProductPage();
+		colors.clickOnBlue();
+		String blueSource = colors.selectedImage();
+		System.out.println(blueSource);
+		colors.clickOnOrange();
+		String orangeSource = colors.selectedImage();
+		System.out.println(orangeSource);
 		
+		Assert.assertNotEquals(blueSource, orangeSource);
 	}
 
 	@AfterTest

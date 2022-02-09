b08803a193110d8fd3c66a7d62ef01ae732aeb48
Jonathan Colegrove <Demetrius472@gmail.com>
2022-02-08 19:50:47 -0500

    initial commit sprint 5

---
+++ sprintautomation/src/test/java/sprint5Pages/ViewCreditSlips.java
@@ -0,0 +1,5 @@
+package sprint5Pages;
+
+public class ViewCreditSlips {
+
+}
---
+++ sprintautomation/src/test/java/sprint5Tests/ViewCreditSlipsTest.java
@@ -0,0 +1,20 @@
+package sprint5Tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class ViewCreditSlipsTest {
+  @Test
+  public void f() {
+  }
+  
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}

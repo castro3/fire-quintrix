0489f9d03a1733b31b0b783b30bd1dbb34dfc55b
Jonathan Colegrove <Demetrius472@gmail.com>
2022-02-09 22:12:40 -0500

    finished test. included code from Sprint 1 to DRY code

--- sprintautomation/src/test/java/sprint5Pages/ViewCreditSlips.java
+++ sprintautomation/src/test/java/sprint5Pages/ViewCreditSlips.java
@@ -1,5 +1,24 @@
 package sprint5Pages;
 
-public class ViewCreditSlips {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import framework.AutomationPage;
+import sprint1Pages.UserLogin;
 
+public class ViewCreditSlips extends AutomationPage {
+	UserLogin user = new UserLogin(driver);
+	
+	public ViewCreditSlips(WebDriver driver) {
+		super(driver);
+	}
+	
+	public void goToMyCreditSlipsPage() {
+		user.navigateToSignInPage();
+		user.signin();
+		this.driver.findElement(By.cssSelector("a[title='My credit slips']")).click();
+	}
+	
+	public String creditSlipHeader() {
+		return this.driver.findElement(By.className("page-heading")).getText();
+	}
 }
--- sprintautomation/src/test/java/sprint5Tests/ViewCreditSlipsTest.java
+++ sprintautomation/src/test/java/sprint5Tests/ViewCreditSlipsTest.java
@@ -1,20 +1,30 @@
 package sprint5Tests;
 
 import org.testng.annotations.Test;
+import framework.TestBase;
+import sprint5Pages.ViewCreditSlips;
 import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
-public class ViewCreditSlipsTest {
+public class ViewCreditSlipsTest extends TestBase {
+	ViewCreditSlips slip;
+	
   @Test
-  public void f() {
+  public void canViewCreditSlip() {
+	  slip.goToMyCreditSlipsPage();
+	  Assert.assertEquals(slip.creditSlipHeader(), "CREDIT SLIPS");
   }
   
   @BeforeTest
   public void beforeTest() {
+	  this.setup();
+	  slip = new ViewCreditSlips(this.getDriver());
   }
 
   @AfterTest
   public void afterTest() {
+	  this.cleanup();
   }
 
 }
